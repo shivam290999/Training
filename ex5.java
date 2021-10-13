75bc0b2aabe5d22eccde9f8c7646372342a32cd3
shivam290999 <92027774+shivam290999@users.noreply.github.com>
2021-10-13 16:26:49 +0530

    Add files via upload

--- ex5.java
+++ ex5.java
@@ -4,24 +4,19 @@ package day13;
 public class ex5 {
 	public static void main(String[] args) {
 		
-		int a[][] = {{1,2,3},{2,3,4},{3,4,5}};
-		int b[][] = {{2,1,3},{2,3,4},{3,4,5}};
-		int c[][] = new int[3][3];
+		int[][] a = {{1,2,3},{2,3,4},{3,4,5}};
+		int[][] b = {{2,1,3},{2,3,4},{3,4,5}};
+		int[][] c = new int[3][3];
 		
 		for(int i=0;i<3;i++) {
 			
 			for(int j=0;j<3;j++) {
 				
 				c[i][j]=a[i][j]+b[i][j];
-			}
-		}
-		for(int i=0;i<3;i++) {
-			
-			for(int j=0;j<3;j++) {
 				
-				System.out.println(c[i][j]+ " ");
+				System.out.print(c[i][j]+ " ");
 			}
-			System.out.println("\n");
+			System.out.println();
 		}
 ; 	}
 
